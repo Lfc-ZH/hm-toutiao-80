ed53d003bcd8d24c75632e576803d50b1b5988a5
lifuchang <1936544634@qq.com>
2019-08-23 10:35:59 +0800

    这次在index.vue中填写了一些样式及结构

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,13 +1,37 @@
 <template>
-    <div class="app">
+  <div class="app">
+    <el-card class="box-card">
+      <img src="../../assets/images/logo_index.png" alt />
 
-    </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
 <style lang="less" scoped>
+.app {
+  background: url(../../assets/images/login_bg.jpg) no-repeat center/ cover;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+.box-card {
+  width: 400px;
+  height: 350px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  img {
+    width: 200px;
+    display: block;
+    margin: 0 auto;
+  }
+}
 </style>
