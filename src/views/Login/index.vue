180c8f7b1ce59cda799c16a3341e7f90cff3e175
lifuchang <1936544634@qq.com>
2019-08-23 11:53:01 +0800

    登录页面结构完成

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -2,13 +2,43 @@
   <div class="app">
     <el-card class="box-card">
       <img src="../../assets/images/logo_index.png" alt />
+      <!-- model="loginForm" 动态绑定model属性  loginForm属性指定当前所有表单元素的数据结合 -->
+    <el-form :model="loginForm" style="margin-top:15px;">
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:236px;margin-right:10px"></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+            <el-checkbox :value="true"> 我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary"  style="width:100%" @click="denglu">登录</el-button>
+        </el-form-item>
 
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    denglu () {
+      alert('王武清是啥掉')
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
